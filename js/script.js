e8205f9642976a6a91eac3f616dd9126ef8eb21f
ButchBet <butchbet01@gmail.com>
2022-08-31 18:10:51 -0500

    Created an array for of objects to identify the inventros, first, last name, year and passed date.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+
+// Some data we can work with
+
+const inventors = [
+    {first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    {first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    {first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    {first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    {first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    {first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    {first: 'Max', last: 'pLANCJ', year: 1858, passed: 1947 },
+];
